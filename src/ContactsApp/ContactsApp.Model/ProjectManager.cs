dd0ae473c463c408cfec6f097d726f5f80ea8405
Nickronus <nickronus@mail.ru>
2023-04-10 23:24:33 +0700

    feature(ProjectManager cl)Добавлен класс сериализации и десериализации

---
+++ src/ContactsApp/ContactsApp.Model/ProjectManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace ContactsApp.Model
+{
+    internal class ProjectManager
+    {
+        private const string _filePath = "C:\\Users\\Nickr\\AppData\\Roaming\\<ваше имя>\\ContactsApp\\ContactsApp.notes";
+
+        public void WriteProjectToJsonFile(Project project)
+        {
+            string json = JsonConvert.SerializeObject(project, Formatting.Indented);
+            File.WriteAllText(_filePath, json);
+        }
+
+        public Project LoadProjectFromJsonFile()
+        {
+            return JsonConvert.DeserializeObject<Project>(_filePath);
+        }
+    }
+}
